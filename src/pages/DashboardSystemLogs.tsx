3342bdd7c717e9718431d4f4bd61125953bd3e59
fallslove23 <fallslove23@gmail.com>
2025-09-07 16:11:36 +0900

    Update DashboardSystemLogs.tsx

--- src/pages/DashboardSystemLogs.tsx
+++ src/pages/DashboardSystemLogs.tsx
@@ -1,12 +1,7 @@
-import { DashboardLayout } from '@/components/DashboardLayout';
 import SystemLogs from './SystemLogs';
 
 const DashboardSystemLogs = () => {
-  return (
-    <DashboardLayout title="시스템 로그" description="시스템 운영 및 관리 관련 로그 모니터링">
-      <SystemLogs />
-    </DashboardLayout>
-  );
+  return <SystemLogs />;
 };
 
 export default DashboardSystemLogs;
