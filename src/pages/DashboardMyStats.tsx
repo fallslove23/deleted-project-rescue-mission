d4bcb5509ff573171d2e7d95ab96e20dfccf76cc
fallslove23 <fallslove23@gmail.com>
2025-09-07 16:16:20 +0900

    Update DashboardMyStats.tsx

--- src/pages/DashboardMyStats.tsx
+++ src/pages/DashboardMyStats.tsx
@@ -1,12 +1,7 @@
-import { DashboardLayout } from '@/components/DashboardLayout';
 import PersonalDashboard from './PersonalDashboard';
 
 const DashboardMyStats = () => {
-  return (
-    <DashboardLayout title="피드백" description="월/반기/연별 개인 성과 분석">
-      <PersonalDashboard />
-    </DashboardLayout>
-  );
+  return <PersonalDashboard />;
 };
 
 export default DashboardMyStats;
