c551c6c06e7bb1ce4bc8cdbce1e89fc4b3c02e38
fallslove23 <fallslove23@gmail.com>
2025-09-07 16:17:38 +0900

    Update DashboardInstructorManagement.tsx

--- src/pages/DashboardInstructorManagement.tsx
+++ src/pages/DashboardInstructorManagement.tsx
@@ -1,12 +1,7 @@
-import { DashboardLayout } from '@/components/DashboardLayout';
 import InstructorManagement from './InstructorManagement';
 
 const DashboardInstructorManagement = () => {
-  return (
-    <DashboardLayout title="강사 관리" description="강사 정보 및 계정 관리">
-      <InstructorManagement showPageHeader={true} />
-    </DashboardLayout>
-  );
+  return <InstructorManagement />;
 };
 
 export default DashboardInstructorManagement;
