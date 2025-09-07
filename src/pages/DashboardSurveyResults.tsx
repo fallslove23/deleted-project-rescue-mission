f217529013d2b573fbc9815a32884f434af66bae
fallslove23 <fallslove23@gmail.com>
2025-09-07 16:12:12 +0900

    Update DashboardSurveyResults.tsx

--- src/pages/DashboardSurveyResults.tsx
+++ src/pages/DashboardSurveyResults.tsx
@@ -1,12 +1,7 @@
-import { DashboardLayout } from '@/components/DashboardLayout';
 import SurveyResults from './SurveyResults';
 
 const DashboardSurveyResults = () => {
-  return (
-    <DashboardLayout title="결과 분석" description="개별 설문 결과 확인 및 공유">
-      <SurveyResults />
-    </DashboardLayout>
-  );
+  return <SurveyResults />;
 };
 
 export default DashboardSurveyResults;
