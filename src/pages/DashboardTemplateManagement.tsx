6fdfff8cb7382d2e031db8730d748297dec4c650
fallslove23 <fallslove23@gmail.com>
2025-09-07 16:11:05 +0900

    Update DashboardTemplateManagement.tsx

--- src/pages/DashboardTemplateManagement.tsx
+++ src/pages/DashboardTemplateManagement.tsx
@@ -1,12 +1,7 @@
-import { DashboardLayout } from '@/components/DashboardLayout';
 import TemplateManagement from './TemplateManagement';
 
 const DashboardTemplateManagement = () => {
-  return (
-    <DashboardLayout title="템플릿 관리" description="설문 템플릿 생성 및 관리">
-      <TemplateManagement showPageHeader={false} />
-    </DashboardLayout>
-  );
+  return <TemplateManagement />;
 };
 
 export default DashboardTemplateManagement;
